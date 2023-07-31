790eec251ab4abd25d3b7e9bd819b96710eec655
yull1860outlook <60593105+yull1860outlook@users.noreply.github.com>
2023-07-31 18:55:04 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,33 +1,3 @@
 #!/bin/bash
 
-echo ""
-echo "Restoring frontend npm packages"
-echo ""
-cd frontend
-apt install npm -y
-npm install
-if [ $? -ne 0 ]; then
-    echo "Failed to restore frontend npm packages"
-    exit $?
-fi
-
-echo ""
-echo "Building frontend"
-echo ""
-npm run build
-if [ $? -ne 0 ]; then
-    echo "Failed to build frontend"
-    exit $?
-fi
-
-cd ..
-#. ./scripts/loadenv.sh
-
-echo ""
-echo "Starting backend"
-echo ""
-python  -m flask run --port=8000 --host=0.0.0.0 --reload --debug
-if [ $? -ne 0 ]; then
-    echo "Failed to start backend"
-    exit $?
-fi
+python  -m flask run --port=8000 --host=0.0.0.0 --reload 
